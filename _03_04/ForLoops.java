56ad063dc227cfff8deca0da6c7e65d14b45f7c5
reinaldopabonfuentes <reinaldopabonfuentes@users.noreply.github.com>
2023-09-29 02:41:11 +0000

    Add last coding exercises

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,19 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
+    for(int counter = 0; counter < 5; counter++ ){
+      System.out.println("I love for loops");
+    }
+    System.out.println();
     // Write a for loop that prints out the numbers 1 to 10
-
+    for (int number = 1; number <= 10; number++){
+      System.out.println(number);
+    }
+    System.out.println();
     // Write a for loop that prints out the numbers 10 to 1
-    
+    for (int number = 10; number >= 1; number--){
+      System.out.println(number);
+    }
 
   }
 
