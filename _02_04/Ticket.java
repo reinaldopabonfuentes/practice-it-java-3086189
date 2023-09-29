e47960a47bc610588c5e9ae50c8c90c5157189c7
reinaldopabonfuentes <reinaldopabonfuentes@users.noreply.github.com>
2023-09-29 02:00:12 +0000

    Add getters & setters to Ticket.java

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -13,7 +13,35 @@ public class Ticket {
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
 
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public void setPrice(double price) {
+    // verify the price
+    if (price < 0.0) {
+      throw new IllegalArgumentException("The price must be >= 0.0");
+    }
+    this.price = price;
+  }
+
+  public void setIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
+
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public boolean getIsReturn() {
+    return isReturn;
+  }
 
 }
